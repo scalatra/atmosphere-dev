7d8d4f421ce177a65340092c8f00f6f730cffdc3
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-09-11 16:32:00 -0700

    try with atmosphere annotation

--- src/main/scala/org/scalatra/atmosphere/ScalatraSocketIOAtmosphereHandler.scala
+++ src/main/scala/org/scalatra/atmosphere/ScalatraSocketIOAtmosphereHandler.scala
@@ -5,7 +5,10 @@ import org.atmosphere.socketio.cpr.SocketIOAtmosphereHandler
 import org.atmosphere.cpr.AtmosphereResource
 import org.atmosphere.socketio.SocketIOSessionOutbound
 import org.atmosphere.socketio.transport.DisconnectReason
+import org.atmosphere.config.service.AtmosphereHandlerService
+import org.atmosphere.interceptor.AtmosphereResourceLifecycleInterceptor
 
+@AtmosphereHandlerService(path = "/socketio", interceptors = Array(classOf[AtmosphereResourceLifecycleInterceptor]))
 class ScalatraSocketIOAtmosphereHandler extends SocketIOAtmosphereHandler {
   def onConnect(event: AtmosphereResource, handler: SocketIOSessionOutbound) {
     println("we got a client")
