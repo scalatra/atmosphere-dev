77f9936b9176257c29cd168ad6cd226aa9a73e4c
Ivan Porto Carrero <ivan@flanders.co.nz>
2012-09-13 15:44:33 -0700

    rename the bootstrap class

--- src/main/scala/Bootstrap.scala
+++ src/main/scala/Bootstrap.scala
@@ -3,7 +3,7 @@ import org.scalatra._
 import org.scalatra.atmosphere._
 import javax.servlet.ServletContext
 
-class Scalatra extends LifeCycle {
+class Bootstrap extends LifeCycle {
   override def init(context: ServletContext) {
     context.enableAtmosphere()
 //    context.mount(new MyScalatraServlet, "/*")
